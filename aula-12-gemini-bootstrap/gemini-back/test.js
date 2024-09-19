aef67b63bc965cba5a164bb58587a256c8f4aa36
Vitor Coelho <vitor.coelho@skf.com>
2024-09-18 22:32:43 -0300

    Adicionando integração com api do gemini

--- aula-12-gemini-bootstrap/gemini-back/test.js
+++ aula-12-gemini-bootstrap/gemini-back/test.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import "dotenv/config";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "O que é um olá mundo?";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
