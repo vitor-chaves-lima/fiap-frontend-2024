cdd0c7a0c30aafd7344fd5af5ab940d048195f13
Vitor Coelho <vitor.coelho@skf.com>
2024-09-18 22:38:58 -0300

    Adicionando api para o prompt

--- aula-12-gemini-bootstrap/gemini-back/test.js
+++ aula-12-gemini-bootstrap/gemini-back/test.js
@@ -1,10 +1,26 @@
 import "dotenv/config";
+
+import Fastify from "fastify";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const fastify = Fastify({
+  logger: true,
+});
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "O que é um olá mundo?";
+fastify.post("/prompt", async function handler(request) {
+  const body = request.body;
+  const prompt = body.prompt;
 
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+  const result = await model.generateContent(prompt);
+
+  return { response: result.response.text() };
+});
+
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
